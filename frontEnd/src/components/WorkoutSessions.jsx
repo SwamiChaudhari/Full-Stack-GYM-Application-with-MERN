5266738dde1d4da941d91cd47b40c0fb722ff78a
swami <swami1642004@gmail.com>
2024-08-10 22:03:40 +0530

    workout

--- frontEnd/src/components/WorkoutSessions.jsx
+++ frontEnd/src/components/WorkoutSessions.jsx
@@ -2,7 +2,47 @@ import React from 'react'
 
 const WorkoutSession = () => {
   return (
-    <div>WorkoutSession</div>
+    <div className="workout_session">
+      <div className="wrapper">
+        <h1>Top Workout Session</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum provident adipisci fuga fugiat,
+           ab inventore itaque nesciunt explicabo deserunt veritatis esse reiciendis incidunt impedit.</p>
+           <img src="/img5.jpg" alt="workout" />
+      </div>
+      <div className="wrapper">
+        <h1>featured bootcamp</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
+          eius eligendi facere doloremque, vero ea minima architecto molestias?</p>
+        <p>
+          <div className="bootcamps">
+            <div>
+             <h4>
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni eaque similique
+             </h4>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias, asperiores non optio similique unde.</p>  
+            </div>
+            <div>
+             <h4>
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni eaque similique
+             </h4>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias, asperiores non optio similique unde.</p>  
+            </div>
+            <div>
+             <h4>
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni eaque similique
+             </h4>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias, asperiores non optio similique unde.</p>  
+            </div>
+            <div>
+             <h4>
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni eaque similique
+             </h4>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias, asperiores non optio similique unde.</p>  
+            </div>
+          </div>
+        </p>
+      </div>
+    </div>
   )
 }
 
