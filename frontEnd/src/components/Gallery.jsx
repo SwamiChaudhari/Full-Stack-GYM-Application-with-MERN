0f41499137278195acc84ece1521e82432cc3bcd
swami <swami1642004@gmail.com>
2024-08-11 10:15:49 +0530

    till-pricing

--- frontEnd/src/components/Gallery.jsx
+++ frontEnd/src/components/Gallery.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 
 const Gallery = () => {
+  const Gallery = ["/img1.webp", "/img2.jpg", "/img3.jpg", "/img4.jpg", "/img5.jpg", "/img6.jpg", "/img6.jpg", "/img8.jpg"]
   return (
-    <div>Gallery</div>
+    <section className="gallery">
+      <h1>Better beats best</h1>
+      <div className="images">
+        <div>
+          {
+            Gallery.slice(0,3).map((element ,index) => (
+              <img key={index} src={element} alt="gallery image" />
+            ))
+          }
+        </div>
+        <div>
+          {
+            Gallery.slice(3,6).map((element ,index) => (
+              <img key={index} src={element} alt="gallery image" />
+            ))
+          }
+        </div>
+        <div>
+          {
+            Gallery.slice(6,9).map((element ,index) => (
+              <img key={index} src={element} alt="gallery image" />
+            ))
+          }
+        </div>
+      </div>
+      
+    </section>
   )
 }
 
