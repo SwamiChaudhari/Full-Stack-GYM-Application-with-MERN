526c8c1f996a3c0a97b19436fc79586e3f479dbc
swami <swami1642004@gmail.com>
2024-08-12 20:37:54 +0530

    finish

--- frontEnd/src/components/Social.jsx
+++ frontEnd/src/components/Social.jsx
@@ -8,16 +8,16 @@ const Social = () => {
 <div class="social-connect">
     <h2>Follow Us on Social Media</h2>
     <div class="social-handlers">
-        <a href="https://github.com/yourprofile" target="_blank" className="social-icon github">
+        <a href="https://github.com/SwamiChaudhari" target="_blank" className="social-icon github">
             <i className="fab fa-github"></i>
         </a>
-        <a href="https://www.twitter.com/yourprofile" target="_blank" class="social-icon twitter">
+        <a href="https://x.com/SwamiChaudhar12" target="_blank" class="social-icon twitter">
             <i class="fab fa-twitter"></i>
         </a>
-        <a href="https://www.instagram.com/yourprofile" target="_blank" class="social-icon instagram">
+        <a href="https://www.instagram.com/swami_77_" target="_blank" class="social-icon instagram">
             <i class="fab fa-instagram"></i>
         </a>
-        <a href="https://www.linkedin.com/in/yourprofile" target="_blank" class="social-icon linkedin">
+        <a href="https://www.linkedin.com/in/swami-chaudhari-a6b781244" target="_blank" class="social-icon linkedin">
             <i class="fab fa-linkedin-in"></i>
         </a>
     </div>
